37a03ebc2b722dd535ac6d04a2e1a080512634a4
2026 <iptv2025tx@gmail.com>
2026-03-05 03:22:13 -0600

    Update combine_remote_playlists_with_epg.py

--- combine_remote_playlists_with_epg.py
+++ combine_remote_playlists_with_epg.py
@@ -6,6 +6,7 @@ import re
 # Add or remove playlist URLs here as needed
 PLAYLISTS = [
         "https://github.com/BuddyChewChew/sports/raw/refs/heads/main/liveeventsfilter.m3u8", 
+        "https://raw.githubusercontent.com/iptv2025tx/one-stop-2026/refs/heads/main/Iptv2025tx/OTT/roxiestreams.m3u8", 
         "https://raw.githubusercontent.com/iptv2025tx/one-stop-2026/refs/heads/main/sinclair.m3u8",      
         "https://raw.githubusercontent.com/iptv2025tx/one-stop-2026/refs/heads/main/Iptv2025tx/Curated/combo-tvp-nc-moj.m3u8", 
         "https://raw.githubusercontent.com/iptv2025tx/one-stop-2026/refs/heads/main/turnerwbd.m3u8", 
@@ -36,6 +37,7 @@ PLAYLISTS = [
         "https://raw.githubusercontent.com/iptv2025tx/one-stop-2026/refs/heads/main/Iptv2025tx/OTT/roku.m3u8", 
         "https://raw.githubusercontent.com/iptv2025tx/one-stop-2026/refs/heads/main/Iptv2025tx/Curated/iheart1.m3u8", 
         "https://raw.githubusercontent.com/iptv2025tx/one-stop-2026/refs/heads/main/Iptv2025tx/OTT/diablo.m3u8",
+        "https://raw.githubusercontent.com/iptv2025tx/one-stop-2026/refs/heads/main/Iptv2025tx/OTT/ott-news.m3u8", 
     # Add more playlists here in the format: "URL_TO_PLAYLIST"
 ]
 
